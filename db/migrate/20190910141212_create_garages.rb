a2025b19f96599e3201eab179dcf191cebcaf502
Alcides Junior <alcidesjunior.infor@gmail.com>
2019-10-22 17:11:26 -0300

    upload de imagens temporaria

--- db/migrate/20190910141212_create_garages.rb
+++ db/migrate/20190910141212_create_garages.rb
@@ -5,9 +5,9 @@ class CreateGarages < ActiveRecord::Migration[5.2]
       t.integer :parking_spaces
       t.integer :busy_space, default: 0
       t.float :price
-      t.string :photo1
-      t.string :photo2
-      t.string :photo3
+      t.text :photo1
+      t.text :photo2
+      t.text :photo3
 
       t.belongs_to :user
       t.timestamps
