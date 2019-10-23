73eda339cc65d626c91e0707025122f4b865fb9c
Alcides Junior <alcidesjunior.infor@gmail.com>
2019-10-23 09:14:39 -0300

    novo login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
+class ApplicationController < ActionController::API
+  def not_found
+    render json: { error: 'not_found' }
+  end
 
-    class ApplicationController < ActionController::API
-      include Knock::Authenticable
-
-      def authorize_as_admin
-        return unauthorized_entity unless !current_user.nil? && current_user.is_admin?
-      end
-
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = JsonWebToken.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { result: "Token invalid." }, status: :unauthorized
     end
+  end
+
+end
