f84ba4d88c55ccb08b655557ba870f6a20dfbacb
Alcides Junior <alcidesjunior.infor@gmail.com>
2019-10-24 10:46:40 -0300

    atualizacao de driver_id e user_id

--- app/controllers/api/v1/parkings_controller.rb
+++ app/controllers/api/v1/parkings_controller.rb
@@ -14,7 +14,7 @@ module Api
         _filter = "all" if _filter.nil?
         if @current_user.role == "ROLE_GD"
           if _filter == "current"
-            parking = Parking.find_by(:user_id=> @current_user.id, :end=>nil)
+            parking = Parking.find_by(:driver_id=> @current_user.id, :end=>nil)
             render json: {result: parking}
           elsif _filter == "all"
             parking = Parking.where(:user_id=> @current_user.id)
