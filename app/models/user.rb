5f091bdcb0bbd62f117038d6996ff39596f2f735
Alcides Junior <alcidesjunior.infor@gmail.com>
2019-11-01 09:50:17 -0300

    atualizacao em user sem cpf

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :document_number, presence: false, uniqueness: true
+  # validates :document_number, presence: false, uniqueness: true
 
   has_many :addresses
   has_many :garages
