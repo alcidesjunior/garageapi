020a6d01f44a2bfdb68c99f74846920260c52f75
Alcides Junior <alcidesjunior.infor@gmail.com>
2019-11-11 09:29:22 -0300

    creating actioncable

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   post '/auth/login', to: 'authentications#login'
+  mount ActionCable.server => '/cable'
   namespace :api do
     namespace :v1 do
       # post 'user_token' => 'user_token#create'
