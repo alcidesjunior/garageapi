92389def6ff24fa92ba02ef9dafc201676d9ae7c
Alcides Junior <alcidesjunior.infor@gmail.com>
2019-11-20 11:07:38 -0300

    testing push notification

--- app/controllers/api/v1/parkings_controller.rb
+++ app/controllers/api/v1/parkings_controller.rb
@@ -32,6 +32,28 @@ module Api
               @parking = Parking.new(parking_params)
 
               if @parking.save
+                players = OneSignal::Player.all(params:{app_id: "d5d9db25-332e-4f14-9dd2-1feec0fbf3cc"})
+                player_id = JSON.parse(players.body)["players"][2]
+
+                notification = OneSignal::Notification.create(params:{
+                  app_id: "d5d9db25-332e-4f14-9dd2-1feec0fbf3cc",
+                  contents:{
+                    en:"#{User.find_by(id: parking_params["user_id"]).name} deseja estacionar"
+
+                  },
+                  ios_category:"PARKING_INVITATION",
+                  buttons:[{id:"1",text:"Acept",icon:"some"},{id:"2",text:"Reject",icon:"some"}],
+                  include_player_ids:[player_id["id"].to_s],
+                  action: "like-btn",
+                  content_available:true,
+                  data:{
+                    name: "Alcides",
+                    idade: 25
+                  }
+                })
+                render json: {
+                  result: "Notication was sended #{player_id['id']}"
+                }
                 garage.busy_space = garage.busy_space + 1
                 garage.save
                 render json: { result: @parking.as_json(:except =>[:user_id])}
@@ -59,6 +81,28 @@ module Api
         @parking = Parking.update(params[:id],parking_params)
         garage = Garage.find_by(:id=>@parking.garage_id)
         if garage.busy_space > 0
+          players = OneSignal::Player.all(params:{app_id: "d5d9db25-332e-4f14-9dd2-1feec0fbf3cc"})
+          player_id = JSON.parse(players.body)["players"][2]
+
+          notification = OneSignal::Notification.create(params:{
+            app_id: "d5d9db25-332e-4f14-9dd2-1feec0fbf3cc",
+            contents:{
+              en:"Se eu tivesse passado vcs nao teria passado - Oliveira, Mateus"
+
+            },
+            ios_category:"PARKING_INVITATION",
+            buttons:[{id:"1",text:"Acept",icon:"some"},{id:"2",text:"Reject",icon:"some"}],
+            include_player_ids:[player_id["id"].to_s],
+            action: "like-btn",
+            content_available:true,
+            data:{
+              name: "Alcides",
+              idade: 25
+            }
+          })
+          render json: {
+            result: "Notication was sended #{player_id['id']}"
+          }
           garage.busy_space = (garage.busy_space - 1)
           garage.save
         end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_144059) do
+ActiveRecord::Schema.define(version: 2019_11_20_114733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 2019_11_13_144059) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "avatar"
+    t.integer "player_id"
   end
 
   create_table "vehicles", force: :cascade do |t|
