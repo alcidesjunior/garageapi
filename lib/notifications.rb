517ca4a6129ebc4a64b657506c1c3bf1101006ac
Alcides Junior <alcidesjunior.infor@gmail.com>
2019-12-02 09:43:08 -0300

    bug fix

--- lib/notifications.rb
+++ lib/notifications.rb
@@ -9,21 +9,23 @@ class Notifications
   def toGarage(garage_owner_id,driver_id,parkingid)
     _driver = User.find_by_id(driver_id)
     _garager = User.find_by_id(garage_owner_id)
-    #puts "[#{_driver.player_id}] o proprietário do carro #{_driver.vehicle.model} de placa #{_driver.vehicle.license_plate} deseja estacionar na sua garagem.\nDeseja permitir?"
-    notification = OneSignal::Notification.create(params:{
-      app_id: @app_id,
-      contents:{
-        en:"O proprietário do carro #{_driver.vehicle.model} de placa #{_driver.vehicle.license_plate} deseja estacionar na sua garagem.\nDeseja permitir?"
-      },
-      ios_category: "PARKING_INVITATION",
-      buttons:[{id:"1",text:"Acept",icon:"some"},{id:"2",text:"Reject",icon:"some"}],
-      include_player_ids:[_garager.player_id],
-      action: "like-btn",
-      content_available:true,
-      data:{
-        parkingId: parkingid.to_s
-      }
-    })
+    if !_driver.nil?
+      #puts "[#{_driver.player_id}] o proprietário do carro #{_driver.vehicle.model} de placa #{_driver.vehicle.license_plate} deseja estacionar na sua garagem.\nDeseja permitir?"
+      notification = OneSignal::Notification.create(params:{
+        app_id: @app_id,
+        contents:{
+          en:"O proprietário do carro #{_driver.vehicle.model} de placa #{_driver.vehicle.license_plate} deseja estacionar na sua garagem.\nDeseja permitir?"
+        },
+        ios_category: "PARKING_INVITATION",
+        buttons:[{id:"1",text:"Acept",icon:"some"},{id:"2",text:"Reject",icon:"some"}],
+        include_player_ids:[_garager.player_id],
+        action: "like-btn",
+        content_available:true,
+        data:{
+          parkingId: parkingid.to_s
+        }
+      })
+    end
   end
 
   def toDriver(user_id, message, status,parkingid)
